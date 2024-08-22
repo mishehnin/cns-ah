07d058291536248a4a74b9c16eae3d28c6d52a1d
Misheh <ningshan2002@126.com>
2024-08-22 16:55:34 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,47 @@
+﻿(function(){
+
+})();
+
+$(function(){
+
+	new FadeAdver({
+		btns : $(".banner .btns span"),
+		divs : $(".banner .pics li"),
+		speed : 5000
+	});
+
+	if($(".hr_mar li").length>=5){
+		new TextSlider({
+			"outer" : $(".hr_mar"),
+			"inner" : $(".hr_mar ul"),
+			direction : "top"
+		});
+	}
+
+	if($(".ad_slider1 li").length>=4){
+		new TextSlider({
+			"outer" : $(".ad_slider1"),
+			"inner" : $(".ad_slider1 ul"),
+			direction : "left"
+		});
+	}
+
+	if($(".ad_slider2 li").length>=4){
+		new TextSlider({
+			"outer" : $(".ad_slider2"),
+			"inner" : $(".ad_slider2 ul"),
+			direction : "left"
+		});
+	}
+
+	$('.hspe_slider ul').carouFredSel({
+			auto : {				
+					timeoutDuration: 5000
+				},
+			direction : "left",
+			prev : ".hspe_slider .pre",
+			next : ".hspe_slider .next",
+			scroll: 1
+		});
+
+});	
